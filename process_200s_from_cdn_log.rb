77c0416f867909fee53f2dbbcc6db7c6b612cebc
Stuart Gale <bishboria@gmail.com>
2015-09-07 17:40:10 +0100

    Explain why sort & uniq are in the code

--- process_200s_from_cdn_log.rb
+++ process_200s_from_cdn_log.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 # Work in progress
 
 # process the cdn log for 200s that are pages on gov.uk, not files (eg, pdf, odt)
@@ -9,6 +10,7 @@ File.readlines("cdn.log").lazy # Suitable for WIP. Really want to stream the dat
   .map {|line| [line[-1],line[-2]]} # only need the status and the basepath
   .select {|(status,basepath)| status == "200"} # all 200s
   .reject {|(status,basepath)| basepath[0..33] == "/government/uploads/system/uploads"} # no files
-  .sort # few options for sorting. Heaviest usage might be best since those pages are priority
+  .sort # heaviest usage might be best since those pages are priority
   .uniq # really want to count occurences
+  # uniq ∘ sort is only needed in the WIP. This will really be pushing stuff to a DB/something else
   .tap {|x| PP.pp x}
