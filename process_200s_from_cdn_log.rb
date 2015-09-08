70aa566b33a5f7614692bb29006a7603012a795d
Stuart Gale <bishboria@gmail.com>
2015-09-08 13:58:34 +0000

    Get rid of unnecessary things
    * db stuff
    * input file name: take data straight from stdin

--- process_200s_from_cdn_log.rb
+++ process_200s_from_cdn_log.rb
@@ -4,40 +4,19 @@
 # process the cdn log for 200s that are pages on gov.uk, not files (eg, pdf, odt)
 
 require 'pp'
-require 'sqlite3'
+require 'csv'
 
-def connect_to_db
-  db = SQLite3::Database.open "basepath200s.db"
-  db.execute "CREATE TABLE IF NOT EXISTS basepaths(basepath TEXT unique, count INT DEFAULT 0)"
-  db
-end
+data = Hash.new(0)
 
-def basepath_accessed(basepath, db)
-  db.execute("insert or ignore into basepaths values (?, 0)", basepath)
-  res = db.execute("select count from basepaths where basepath=?", basepath)
-  db.execute("update basepaths set count = ? where basepath = ?", res[0][0]+1, basepath)
-end
+$stdin.readlines
+  .map(&:split)
+  .map {|line| [line[-1],line[-2]]} # only need the status and the basepath
+  .select {|status,basepath| status == "200"} # all 200s
+  .reject {|status,basepath| basepath[0..33] == "/government/uploads/system/uploads"} # no files
+  .each {|status,basepath| data[basepath] += 1}
 
-def show_db_state(db)
-  db.execute("select * from basepaths order by count DESC") do |row|
-    PP.pp row
+CSV.open("out.csv","w") do |csv|
+  data.each do |basepath, count|
+    csv << [basepath, count]
   end
-end
-
-begin
-  db = connect_to_db
-
-  File.readlines("cdn.log") # Suitable for WIP. Really want to stream the data
-    .map(&:split)
-    .map {|line| [line[-1],line[-2]]} # only need the status and the basepath
-    .select {|(status,basepath)| status == "200"} # all 200s
-    .reject {|(status,basepath)| basepath[0..33] == "/government/uploads/system/uploads"} # no files
-    .each {|(status,basepath)| basepath_accessed(basepath, db)}
-
-  show_db_state(db)
-
-rescue SQLite3::Exception => e
-  puts e
-ensure
-  db.close if db
 end
