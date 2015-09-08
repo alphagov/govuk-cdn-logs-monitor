af55a5a1ff316250e4b765bc0fc4e61cd33aaf80
Stuart Gale <bishboria@gmail.com>
2015-09-08 16:25:14 +0000

    remove comment from shell script

--- nightly_run.sh
+++ nightly_run.sh
@@ -1,4 +1,4 @@
-infile=$(ls | sort -n -t _ -k 2 | tail -1) #| awk -F_ '{ print $1 "_" print $2 }')
+infile=$(ls | sort -n -t _ -k 2 | tail -1)
 outfile=$(echo "$infile" | awk -F_ '{print $2}' | awk -F. '{print $1}')
 echo "This process is creating $outfile.csv"
 cat $infile | ruby process_200s_from_cdn_log.rb "$outfile.csv"
