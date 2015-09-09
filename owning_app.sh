b4907bde2c490b930446f4e732eac3c80cb0aba6
Stuart Gale <bishboria@gmail.com>
2015-09-09 15:58:38 +0100

    Show the owning app for a given basepath

---
+++ owning_app.sh
@@ -0,0 +1,4 @@
+# experiment: only works for html pages
+read basepath
+content=$(curl -s "https://www.gov.uk$basepath")
+echo $content | sed -n -e 's/^.*govuk:rendering-application\" content=\"\([^\"]*\).*/\1/p'
