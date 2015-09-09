a7fdc6831d91448cbe381254552db9b85ca3f015
Stuart Gale <bishboria@gmail.com>
2015-09-09 13:09:08 +0100

    Add explanatory comments to the shell scripts

--- nightly_run.sh
+++ nightly_run.sh
@@ -1,5 +1,11 @@
+# accept an output directory to store the processed logs
 outdirectory=$1
+
+# find the newest file
 infile=$(ls | sort -n -t _ -k 2 | tail -1)
+
+# expect name to be formatted: cdn-govuk.log-YYYYMMDD.gz
+# extract YYYYMMDD part of newest file
 outfile=$(echo "$infile" | awk -F_ '{print $2}' | awk -F. '{print $1}')
 
 echo "This process is creating $outdirectory/$outfile.csv"
--- process_gz_logs.sh
+++ process_gz_logs.sh
@@ -1,5 +1,9 @@
+# accept an output directory to store the processed logs
 outdirectory=$1
+
 for f in *.gz; do
+    # expect name to be formatted: cdn-govuk.log-YYYYMMDD.gz
+    # extract YYYYMMDD part of zipped file name
     outfile=$(echo $f | awk -F- '{print $3}' | awk -F. '{print $1}')
 
     echo "creating $outdirectory/$outfile.csv"
