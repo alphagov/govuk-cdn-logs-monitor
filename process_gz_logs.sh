614b4c693d84c3c34dbf1ebb9772ea72ea241aea
Stuart Gale <bishboria@gmail.com>
2015-09-09 12:57:34 +0100

    Script to process zipped log files
    * Accepts an output directory
    * Leaves the original zipped files intact

---
+++ process_gz_logs.sh
@@ -0,0 +1,7 @@
+outdirectory=$1
+for f in *.gz; do
+    outfile=$(echo $f | awk -F- '{print $3}' | awk -F. '{print $1}')
+
+    echo "creating $outdirectory/$outfile.csv"
+    gunzip $f -c | ruby process_200s_from_cdn_log.rb "$outdirectory/$outfile.csv"
+done
