45defb64e23e19bf5fa4f2ed27aef3c3a21c8093
Stuart Gale <bishboria@gmail.com>
2015-09-09 10:06:06 +0000

    Add output directory parameter to nightly script

--- nightly_run.sh
+++ nightly_run.sh
@@ -1,4 +1,6 @@
+outdirectory=$1
 infile=$(ls | sort -n -t _ -k 2 | tail -1)
 outfile=$(echo "$infile" | awk -F_ '{print $2}' | awk -F. '{print $1}')
-echo "This process is creating $outfile.csv"
-cat $infile | ruby process_200s_from_cdn_log.rb "$outfile.csv"
+
+echo "This process is creating $outdirectory/$outfile.csv"
+cat $infile | ruby process_200s_from_cdn_log.rb "$outdirectory/$outfile.csv"
