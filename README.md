9e9c5cd8fe29de520c1c5b19a8835e5670dca8f7
Stuart Gale <bishboria@gmail.com>
2015-09-09 14:38:49 +0100

    Check if the output file exists first in batch script

--- README.md
+++ README.md
@@ -7,7 +7,5 @@ See <https://gov-uk.atlassian.net/wiki/display/FS/RFC+21%3A+Monitoring+for+404s>
 
 # To do
 
-1. nightly script that accepts the destination folder for processed log files
-2. script that processes all the zipped (archived) files
-3. given two output files, produce the intersection: find the common basepaths
+1. given two output files, produce the intersection: find the common basepaths
 ...
--- process_gz_logs.sh
+++ process_gz_logs.sh
@@ -6,6 +6,11 @@ for f in *.gz; do
     # extract YYYYMMDD part of zipped file name
     outfile=$(echo $f | awk -F- '{print $3}' | awk -F. '{print $1}')
 
-    echo "creating $outdirectory/$outfile.csv"
-    gunzip $f -c | ruby process_200s_from_cdn_log.rb "$outdirectory/$outfile.csv"
+    # stop processing this file if the output already exists
+    if [ -f "$outdirectory/$outfile.csv" ]; then
+        echo "$outdirectory/$outfile.csv already exists"
+    else
+        echo "creating $outdirectory/$outfile.csv"
+        gunzip $f -c | ruby process_200s_from_cdn_log.rb "$outdirectory/$outfile.csv"
+    fi
 done
