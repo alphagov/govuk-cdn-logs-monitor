a7f896671ded4e982b53596ae0bc3087361f23c9
Stuart Gale <bishboria@gmail.com>
2015-09-14 10:01:17 +0000

    Add ruby code for accumulating counts of http 200s

---
+++ accumulate_into_master.rb
@@ -0,0 +1,24 @@
+require 'csv'
+
+masterlist = ARGV[0]
+_200s_directory = ARGV[1]
+accumulator = Hash.new(0)
+
+# read in current master list
+CSV.foreach(masterlist, 'r') do |row|
+  accumulator[row[0]] += row[1]
+end
+
+# update frequency of 200 responses
+Dir["#{_200s_directory}.csv"].each do |csv|
+  CSV.foreach(csv, 'r') do |row|
+    accumulator[row[0]] += row[1]
+  end
+end
+
+# write out updated master list
+CSV.open(masterlist, 'w') do |csv|
+  accumulator.each do |k,v|
+    csv << [k, v]
+  end
+end
