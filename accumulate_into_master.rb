2120562776d945ebfbdbe536f65af1dbed539915
Stuart Gale <bishboria@gmail.com>
2015-09-14 16:40:18 +0000

    Update algorithm for adding new urls to the masterlist

--- accumulate_into_master.rb
+++ accumulate_into_master.rb
@@ -2,21 +2,34 @@ require 'csv'
 
 masterlist = ARGV[0]
 _200s_directory = ARGV[1]
-accumulator = Hash.new(0)
+accumulator = []
 
 # read in current master list
 CSV.foreach(masterlist, 'r') do |row|
-  accumulator[row[0]] += row[1].to_i
+  accumulator << row[0]
 end
 
-# update frequency of 200 responses
-Dir["#{_200s_directory}/*.csv"].each do |csv|
-  CSV.foreach(csv, 'r') do |row|
-    accumulator[row[0]] += row[1].to_i
+day_zeroes = Dir["#{_200s_directory}/*.csv"]
+week_later = day_zeroes[7..-1] || []
+
+# intersection of urls 1 week apart get added to the masterlist
+day_zeroes.zip(week_later).each do |start,_end|
+  if _end.nil?
+    break
   end
+  a = []; CSV.foreach(start,'r') do |row|
+    a << row[0]
+  end
+  b = []; CSV.foreach(_end,'r') do |row|
+    b << row[0]
+  end
+  accumulator += (a & b)
 end
 
 # write out updated master list
+accumulator.uniq!
+accumulator.sort!
+
 CSV.open(masterlist, 'w') do |csv|
   accumulator.each do |k,v|
     csv << [k, v]
