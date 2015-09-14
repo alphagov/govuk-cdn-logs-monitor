a797a60c5b2630aaaf70f4b3b9aac4796709022b
Stuart Gale <bishboria@gmail.com>
2015-09-14 13:33:08 +0000

    Convert strings to numbers first...

--- accumulate_into_master.rb
+++ accumulate_into_master.rb
@@ -6,13 +6,13 @@ accumulator = Hash.new(0)
 
 # read in current master list
 CSV.foreach(masterlist, 'r') do |row|
-  accumulator[row[0]] += row[1]
+  accumulator[row[0]] += row[1].to_i
 end
 
 # update frequency of 200 responses
-Dir["#{_200s_directory}.csv"].each do |csv|
+Dir["#{_200s_directory}/*.csv"].each do |csv|
   CSV.foreach(csv, 'r') do |row|
-    accumulator[row[0]] += row[1]
+    accumulator[row[0]] += row[1].to_i
   end
 end
 
