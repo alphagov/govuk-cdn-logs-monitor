ebb0ea41c9cebad858adb8aa8962fe3e8be3aa0a
Stuart Gale <bishboria@gmail.com>
2015-09-15 11:36:25 +0100

    Notify statsd of a 404.
    Print to stdout relevant details of the 404 in json format.

--- alert_if_404_url_present.rb
+++ alert_if_404_url_present.rb
@@ -1,4 +1,17 @@
 require 'csv'
+require 'statsd-ruby'
+require 'json'
+
+statsd_client = Statsd.new("localhost")
+statsd_client.namespace = ENV["GOVUK_STATSD_PREFIX"]
+
+def date(fragment)
+  "#{fragment[-8]} #{fragment[-7]} #{fragment[-6]}"
+end
+
+def time(fragment)
+  "#{fragment[-5]} #{fragment[-4]}"
+end
 
 known_good = []
 CSV.foreach("masterlist.csv", 'r') do |row| # assume each line sorted by basepath
@@ -15,7 +28,11 @@ while line = gets do
 
   _404_path = fragment[-2]
   if known_good.any? {|x| x == _404_path}
-    puts "************** ALERT #{_404_path} is 404ing"
+    statsd_client.increment('404')
+    result = { url: _404_path, method: fragment[-3],
+               time: time(fragment), date: date(fragment)
+             }
+    puts JSON.generate(result)
     next
   end
 end
