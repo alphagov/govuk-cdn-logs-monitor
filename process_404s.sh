0f4a0369a3083ed7aef87c0cc44198f7027698eb
Stuart Gale <bishboria@gmail.com>
2015-09-16 11:38:27 +0100

    Calculate newest log file from the given source directory

--- process_404s.sh
+++ process_404s.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-read file
-masterfile="${1}"
+srcdirectory="${1}"
+masterfile="${2}"
+
+srcfile=$(ls -1tr "$srcdirectory" | tail -1)
 
 # -F handles log rotations
 # -c +0 outputs the entire file (not just last ten lines)
 # -c -0 watches the very end of the file
-tail -F -c -0 "$file" | ruby alert_if_404_url_present.rb "$masterfile"
+tail -F -c -0 "$srcdirectory/$srcfile" | ruby alert_if_404_url_present.rb "$masterfile"
