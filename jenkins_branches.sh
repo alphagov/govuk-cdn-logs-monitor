6287f13b206e646d8a8e2da4737afa9870f54f76
Richard Boulton <richard.boulton@digital.cabinet-office.gov.uk>
2015-12-04 14:45:06 +0000

    Remove python environment setup
    This is a ruby project.

--- jenkins_branches.sh
+++ jenkins_branches.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -e
 
-VENV_PATH="${HOME}/venv/${JOB_NAME}"
-
-[ -x ${VENV_PATH}/bin/pip ] || virtualenv ${VENV_PATH}
-. ${VENV_PATH}/bin/activate
-
-pip install -q ghtools
-
 REPO="alphagov/govuk-cdn-logs-monitor"
 gh-status "$REPO" "$GIT_COMMIT" pending -d "\"Build #${BUILD_NUMBER} is running on Jenkins\"" -u "$BUILD_URL" >/dev/null
 
