b595a33100fc0f402e13c069b4ba8d35eca609b8
Richard Boulton <richard.boulton@digital.cabinet-office.gov.uk>
2015-12-08 18:02:42 +0000

    Set locale for external command
    Set the `LC_ALL` environment variable to `C` to use the standard C
    locale for external commands.  This ensures, for example, that sort
    performs a bytewise comparison, and is consistent between environments.

--- lib/process_streamer.rb
+++ lib/process_streamer.rb
@@ -2,7 +2,7 @@ require 'open3'
 
 class ProcessStreamer
   def self.open(command)
-    Open3.popen2(*command) do |_stdin, stdout, wait_thr|
+    Open3.popen2({"LC_ALL" => "C"}, *command) do |_stdin, stdout, wait_thr|
       begin
         yield stdout
       rescue
